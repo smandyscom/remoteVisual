7bcf2ca7e421769a075935dd56e94c82fd51ff60
smandyscom <smandyscom@gmail.com>
2016-05-31 12:10:11 +0800

    added socket broadcast

--- program/joystickXudp.py
+++ program/joystickXudp.py
@@ -1,5 +1,15 @@
 from time import sleep
 import pygame
+from socket import *
+# import socket
+#socket part
+HOST = "127.0.0.1"
+PORT = 5001
+sender = socket(AF_INET,SOCK_DGRAM)
+# sender.bind((HOST,PORT))
+sender.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+sender.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
 #in order to drive event.get , need to initialize base module
 pygame.init()
 pygame.joystick.init()
@@ -9,9 +19,12 @@ joystick = pygame.joystick.Joystick(axisIndex)
 joystick.init()
 print ("Joystick initialize status {0}".format(joystick.get_init()))
 print ("Joystick name {0}".format(joystick.get_name()))
+
 var=1
 while var == 1:
    pygame.event.get()
    print ("Axis value {:>6.3f}".format(joystick.get_axis(0)))
+   # sender.sendto("Value {:>6.3f}".format(joystick.get_axis(0)) ,('255.255.255.255',PORT))
+   sender.sendto("Go",('255.255.255.255',PORT))
    sleep(0.1)
 
