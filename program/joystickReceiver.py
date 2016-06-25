4bbc6f851e5dec62125202f096df47c6a0b5bf3e
root <root@raspberrypi.(none)>
2016-06-25 10:43:08 +0800

    tested on py

--- program/joystickReceiver.py
+++ program/joystickReceiver.py
@@ -3,7 +3,7 @@ import pygame
 import joystickCommandPack
 import objectTransmission
 
-receiver = objectTransmission.objReceiver(('127.0.0.1', 5001))
+receiver = objectTransmission.objReceiver(('', 5001))
 
 while 1:
     print receiver.recvfrom()
