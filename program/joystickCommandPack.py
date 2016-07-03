b8ff4100af059f153e427f9c929a2558a1e1b7aa
smandyscom <smandyscom@gmail.com>
2016-07-03 12:47:05 +0800

    added servo command wrapper

--- program/joystickCommandPack.py
+++ program/joystickCommandPack.py
@@ -6,4 +6,6 @@ class commandPack:
         self.buttonValues = [0, 0]
     def __str__(self):
         return "%s;%s" % (self.axisValues, self.buttonValues)
+    def toServoCommand(self):
+        return [(value + 2) * 750 for value in self.axisValues]
 
