fdf6225f702a223becebbe4fa2ffd82228b84b34
smandyscom <smandyscom@gmail.com>
2016-07-04 23:10:54 +0800

    command pack updated

--- program/joystickCommandPack.py
+++ program/joystickCommandPack.py
@@ -1,11 +1,11 @@
 class commandPack:
-    axisValues = [0.0, 0.0]
-    buttonValues = [0, 0]
     def __init__(self):
         self.axisValues = [0.0, 0.0]
         self.buttonValues = [0, 0]
     def __str__(self):
         return "%s;%s" % (self.axisValues, self.buttonValues)
-    def toServoCommand(self):
+    def axis2Servo(self):
         return [(value + 2) * 750 for value in self.axisValues]
+    def axis2Pwm(self):
+        return [abs(value) * 1000000 for value in self.axisValues]
 
