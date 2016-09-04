74f23208c953fa53649aa8e6e17858b8bfa61ee0
smandyscom <smandyscom@gmail.com>
2016-09-04 14:59:20 +0800

    added axis control part

--- program/server.py
+++ program/server.py
@@ -1,9 +1,13 @@
 import BaseHTTPServer
 import SimpleHTTPServer
-
+import buttonIncrementor
+import pigpio
 FILE = 'jquery.html'
 PORT = 8060
 
+axis1 = buttonIncrementor.buttonIncrementor()
+axis2 = buttonIncrementor.buttonIncrementor()
+piController = pigpio.pi()
 
 class TestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         """The test example handler."""
@@ -14,6 +18,16 @@ class TestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             # self.wfile.write(data_string) #as response
             self.wfile.write("server received") #as response
             print 'received:{0}'.format(data_string)
+            if data_string == "up":
+                axis1.move(True)
+            if data_string == "down":
+                axis1.move(False)
+            if data_string == "left":
+                axis2.move(True)
+            if data_string == "right":
+                axis2.move(False)
+            piController.set_servo_pulsewidth(16, axis1.currentPosition)
+            piController.set_servo_pulsewidth(20, axis2.currentPosition)
 
 def start_server():
     """Start the server."""
